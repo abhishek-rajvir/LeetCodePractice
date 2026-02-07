08065dfe207c6b75a90206a371b8f602959e263d
Abhishek Rajvir <95771258+abhishek-rajvir@users.noreply.github.com>
2026-02-08 00:58:06 +0530

    8-2-26 Implement 3 solutions for Two Sum problem
    Added multiple solutions for the Two Sum problem in Java and C++. Included comments on runtime and memory complexity for each solution.

---
+++ 1. Two Sum.java
@@ -0,0 +1,134 @@
+// Here i have provided 3 solutions for the problem [2 in java and 1 in c++]
+
+// Solution 1 [HashMap avg tc and avg sc ]
+
+// Runtime
+// 10
+// ms
+// Beats
+// 45.99%
+// Analyze Complexity
+// Memory
+// 47.38
+// MB
+// Beats
+// 19.09%
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        // intialising variable
+        int i = 0, j = 1;
+
+        // create a map data(can be duplicate so value is list) as index and position as value
+        Map<Integer,List<Integer>> hm = new HashMap<>();
+
+        // add all to map
+        for (int k = 0; k < nums.length; k++) {
+            List<Integer> ls = hm.get(nums[k]);
+            if(ls == null){
+                ls = new ArrayList<>(Arrays.asList(k));
+                hm.put(nums[k], ls);
+                continue;
+            }
+            ls.add(k);
+        }
+
+        // iterate and find diff in hashset
+        for (; i < nums.length; i++) { 
+            List<Integer> ls;
+            ls = hm.get(target-nums[i]);
+            if(ls!=null){
+                // if the number is 6[ 3 + 3 is possible] but 
+                // the array has [3,2,4] then check if the map has duplicates
+                if(nums[i]==target-nums[i] && ls.size()==1){
+                    continue;
+                }
+                // get the last 0 or 1 position
+                j = ls.getLast();
+                // if found no need to iterate ahead
+                break;
+            }
+        }
+        return new int[] { i, j };
+    }
+}
+
+// solution 2  [bruteforce avg tc and avg sc ]
+
+// Runtime
+// 37
+// ms
+// Beats
+// 41.29%
+// Analyze Complexity
+// Memory
+// 47.27
+// MB
+// Beats
+// 35.24%
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0,j=0;
+        
+        for(;i<nums.length;i++){
+            j = findElement(nums,i,target);
+            if(j>-1){
+                break;
+            }
+        }
+        return new int[] {i,j};
+    }
+
+    public int findElement(int[] arr,int start,int target){
+        for(int i = start+1;i<arr.length;i++){
+            if(arr[i]+arr[start] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// solution 3 [ C++ best sc worst tc]
+
+// Runtime
+// 155
+// ms
+// Beats
+// 5.04%
+// Analyze Complexity
+// Memory
+// 13.99
+// MB
+// Beats
+// 93.64%
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ls1;
+        bool done = false;
+        for (int j=0;j<nums.size();j++){
+            if (done){
+                break;
+            }
+            else {
+                for (int i=0;i<nums.size();i++){
+                    if (j == i){continue;}
+                    else if (nums[j] + nums[i] == target){
+                        ls1.push_back(j);
+                        ls1.push_back(i);
+                        done = true;
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+            }
+        }
+        return ls1;
+    }
+};
